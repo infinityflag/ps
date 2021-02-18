33488e1f0b12d120715f56d48dcc0454067c0e3b
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-02-18 17:21:39 +0900

    Update cru.cpp

--- cru.cpp
+++ cru.cpp
@@ -1,7 +1,32 @@
-#include<iostream>
+//
+//  main.cpp
+//  cp practice
+//
+//  Created by jm on 2021/02/16.
+//
+
+#include <iostream>
+#include <algorithm>
 using namespace std;
+
+void dfs(int a[],vector<int> v[],int u, int p){
+    dp[u] = a[u-1];
+    
+    for(auto child: v[u]){
+        if(child == p){
+            continue;
+        }
+        dfs(a,v,child,u);
+        maxx = max(maxx, dp[child]);
+    }
+    dp[u] += maxx;
+}
+
 
 int main()
 {
-  cout << 1;
- }
+    
+    dfs(1,0);
+    
+    
+}
