7d3ac4c0652288a9debcf3bfa122aebbfe239a77
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-02-21 17:11:54 +0900

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -48,3 +48,63 @@ int main()
 
     }
 }
+
+
+
+
+later...
+    
+    
+    
+    
+   #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+#include <cmath>
+using namespace std;
+#define maxn 21
+
+int arr[1000001];
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        for(int i=0;i<=10;i++){
+            cnt[i] = 0;
+        }
+        int a,b;
+        cin >> a >> b;
+        int tmp = 1;
+        for(int i=1;i<=b;i++){
+            tmp = (tmp*a)%10;
+            if(tmp == 0) tmp = 10;
+            //cout << "tmp: " << tmp << '\n';
+            //cnt[tmp]++;
+            //if(cnt[tmp]>=2){
+              //break;
+            //}
+            arr[i] = tmp;
+        }
+        //와! 정말 구현이 꼬이는구나!
+        /*
+        for(int ii = 1; ii<i;ii++){
+            cout << arr[ii] << ' ';
+        }
+        cout <<'\n';
+        */
+        //cout << max(arr[b%(i-1)], arr[(i-1)%b]) << '\n';
+        cout << arr[b]<<'\n'; 
+    }
+}
