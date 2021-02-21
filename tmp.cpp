77cd1e772e12f41b24c31a26f5dd855d6ed34dfd
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-02-21 17:43:31 +0900

    Create tmp.cpp

---
+++ tmp.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+#include <cmath>
+using namespace std;
+#define maxn 21
+
+//int arr[1000001];
+int main()
+{
+    int n; 
+    cin >> n;
+    for(int i=0;i<6;i++){
+        if(a[i] ){
+            
+        }
+
+    }
+    while(i == n){
+         
+    }
+
+    1 6 : 1 -6
+    7 18 : 2 - 12
+    19 36 : 3 -16
+    37 60 : 4 - 60-38= 22
+    총 6개씩 많아지는 조건이 있음
+
+}
