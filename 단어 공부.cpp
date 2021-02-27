b0fb7ea7a812133c89e4d531bdd4e372da23baf9
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-02-27 13:49:36 +0900

    second try

--- 단어 공부.cpp
+++ 단어 공부.cpp
@@ -33,5 +33,61 @@ int main() {
             ans = i;
         }
     }
+    
+    
+    
+    second try
+        
+        
+        #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+using namespace std;
+typedef long long ll;
+int chk[1000];
+int main() {
+    string s;
+    cin >> s;
+    for(int i=0;i<s.size();i++){
+        if('a'<=s[i] &&s[i] <='z'){
+            s[i] = s[i] - 32;
+        }   
+    }
+    for(int i=0;i<s.size();i++){
+        chk[s[i]]++;
+    }
+    int maxc = -1e9;
+    char ans; 
+    int ok = 0; 
+    for(int i='A';i<='Z';i++){
+        if(maxc<chk[i]){
+            maxc = chk[i];
+            ans = i;
+        }
+    }
+    int c = 0;
+    for(int i='A';i<='Z';i++){
+        if(maxc == chk[i]){
+            c++; 
+        }   
+    }
+    if(c > 1){
+        cout << '?';
+    }
+    else{
+        cout << ans;
+    }
+}
+
     cout << ans;
 }
