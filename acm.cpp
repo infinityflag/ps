be84034d8efaf0318c2d9c41cd94ddb7aafc4304
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-03-04 22:50:13 +0900

    Update acm.cpp

--- acm.cpp
+++ acm.cpp
@@ -101,3 +101,51 @@ int main() {
         cout << ans <<'\n';
     }
 }
+
+
+third try
+    #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+using namespace std;
+int chk[10001];
+int a[100][100];
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int h,w,target;
+        cin >> h >> w>>target;
+        int cnt = 0;
+        for(int j=1;j<=w;j++){
+            for(int i=h;i>=1;i--){
+                a[i][j] = ++cnt;
+            }
+        }
+	
+        int ans=0;
+        for(int i=1;i<=h;i++){
+            for(int j=1;j<=w;j++){
+                if(a[i][j] == target){
+                    ans = 100*(h-i+1)+j;
+                    break;
+                    //cout << ans;
+                    //return 0;
+                }
+            }
+        }
+        cout << ans <<'\n';
+
+    }
+}
+correct!
