4925c4d2224a3e503cc0e0aa8eda147f8f742633
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-03-04 23:57:52 +0900

    Create _base conversion.cpp

---
+++ _base conversion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+using namespace std;
+char a[100];
+int main() {
+    int t = 'A';
+    for(int i=10;i<=36;i++){
+        a[i] = t;
+        t++;
+    }
+    
+    string s;
+    int b;
+    cin>>s>>b;
+    while(n%b>0){
+        
+    }
+}
