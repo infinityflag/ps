f9236ec2e694a7ad5d7a3d9307a51bb3e9c14592
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-03-05 21:58:10 +0900

    Update _base conversion.cpp

--- _base conversion.cpp
+++ _base conversion.cpp
@@ -10,20 +10,30 @@
 #include <vector>
 #include <stdlib.h>
 #include <cstring>
+#include <cmath>
+#include <math.h>
 #include <string>
+typedef long long ll;
 using namespace std;
 char a[100];
+int convert(char c){
+    if('0' <= c && c <= '9') return c-'0';
+    else return c-'A'+10;
+}
 int main() {
-    int t = 'A';
-    for(int i=10;i<=36;i++){
+    int t = 10;
+    for(int i='A';i<='Z';i++){
         a[i] = t;
         t++;
     }
-    
     string s;
     int b;
     cin>>s>>b;
-    while(n%b>0){
-        
+    ll sum = 0;
+    int power 
+    for(int i=s.size()-1;i>=0;i--){
+        sum += pow(b,i)*convert(s[i]);
     }
+    cout << sum;
+    if(arr[i] == s)
 }
