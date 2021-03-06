52dbe42ab72b236378decbd8e8456e218845bc2c
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-03-06 12:22:31 +0900

    Create _base conversion2.cpp

---
+++ _base conversion2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <cmath>
+#include <math.h>
+#include <string>
+typedef long long ll;
+using namespace std;
+//char a[100];
+int cnt[100];
+int check(ll n){
+    if(0 <= n && n <=9){
+        return n;
+    }
+    else{
+        n+'A'
+    }
+    c-'A'+10 == n
+    return c;
+}
+int main() {
+
+    ll n;
+    int b;
+    ll t = n;
+    while(t){
+        t % b
+        t/=b;
+    }
+}
