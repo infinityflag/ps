add3dcda7240727cea59450f0d829629b4d5f7c6
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-03-14 11:43:51 +0900

    Update coordinate_sorting.cpp

--- coordinate_sorting.cpp
+++ coordinate_sorting.cpp
@@ -80,3 +80,51 @@ int main()
     }
 }
 
+
+third try -> ac
+
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+#include <cmath>
+using namespace std;
+#define maxn 21
+
+//int arr[1000001];
+
+bool cmp(pair<int,int>&a,pair<int,int>&b){
+    if(a.first == b.first){
+        return a.second < b.second;
+    }
+    return a.first < b.first;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n;
+    cin >> n;
+    vector<pair<int,int> > v(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i].first >> v[i].second;
+        //int x,y;
+        //cin >> x >> y; 
+        //v.push_back({x,y});
+    }
+    sort(v.begin(), v.end(),cmp);
+    for(auto i: v){
+        cout << i.first << ' '<<i.second<<'\n';
+    }
+}
+
+
