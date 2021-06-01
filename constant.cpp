44d506bc0c1bbf27b229d3e64ac4fd813fef0b16
infinityflag <79241687+infinityflag@users.noreply.github.com>
2021-06-01 23:14:41 +0900

    Update constant.cpp

--- constant.cpp
+++ constant.cpp
@@ -9,34 +9,21 @@ int main() {
 	cin >> n >> k;
 		vector<int> v,v2;
 		int t = n;
-		while(n){
+		while(t){
 			v.push_back(t%10);
 			t /= 10;
 		}
-		int sum1 = 0, sum2=0;
-		for(int i=v.size()-1;i>=0;i--){
-			if(i==0){
-				sum1 += v[i];
-			}
-			else{
-				sum1 += (10*i)*v[i];
-			}
-		}
+		int g1 = 0;
+        for(int d:v){
+            g1 = 10*g1+d;
+        }
 		int t2 = k;
-		while(k){
+        while(t2){
 			v2.push_back(t2%10);
 			t2 /= 10;
 		}
-		for(int i=v2.size()-1;i>=0;i--){
-			if(i==0){
-				sum2 += v[i];
-			}
-			else{
-				sum2 += (10*i)*v[i];
-			}
+		int g2 = 0;
+		for(int d:v2){
+            g2 = 10*g2+d;
 		}
-
-        cout << (sum1>sum2?sum1:sum2);
-}
-상수 분해합
-
+		cout << (g1>g2? g1:g2);}
